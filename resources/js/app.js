156a31a2a2cb0e30448319d37f4c6d8e2f6a7f0e
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2025-11-07 16:09:43 +0600

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap'; // optional
+import { createApp } from 'vue';
+import App from './app.vue';
+
+createApp(App).mount('#app'); // div id must match!
---
+++ resources/js/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome to IronNest!</h1>
+    <p>This is your Vue frontend.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #0e404f;
+}
+</style>
